e702c507525c81fe4bca7eea18e2240098c9a53d
Sergio Espada Rubio <30748831+EspadaSer@users.noreply.github.com>
2019-09-01 18:16:57 +0200

    Create PUBGMovieDeleteV1.py
    Initial Release

---
+++ V1/PUBGMovieDeleteV1.py
@@ -0,0 +1,16 @@
+import os
+
+# Initialization. Do not modify
+i=0
+folder_path = "C:\Steam\SteamApps\common\PUBG\TslGame\Content\Movies"
+filename = ["LicenseScreen.mp4", "LoadingScreen.mp4", "LoadingScreen_Xbox.mp4"]
+
+# Main
+
+for i in range(3):
+    filepath = folder_path + "\\" + filename[i]
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+        print("Deleted " + filepath)
+    else:
+        print("File not found " + filepath)
